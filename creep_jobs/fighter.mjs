1f783b091b7728c0376d9f107d196380a52c9874
Connor Cahoon <connor.cahoon@gmail.com>
2025-12-27 16:28:51 -0600

    Simplify fighter body and reduce cost

--- creep_jobs/fighter.mjs
+++ creep_jobs/fighter.mjs
@@ -3,8 +3,8 @@ import { TOUGH, ATTACK, MOVE, ERR_NOT_IN_RANGE} from 'game/constants';
 import { Creep, StructureSpawn, StructureRampart } from 'game/prototypes';
 
 // Body configuration for fighter creeps
-export const FIGHTER_BODY = [MOVE, MOVE, ATTACK, ATTACK];
-export const FIGHTER_COST = 260; // 50 + 50 + 80 + 80
+export const FIGHTER_BODY = [MOVE, ATTACK];
+export const FIGHTER_COST = 130; // 50 + 80
 
 export function act_fighter(creepInfo, controller, winObjective) {
     const creep = getObjectById(creepInfo.id);
